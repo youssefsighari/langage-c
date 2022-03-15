4fb57a6fc5196e9450c4c2e1092fe12b02268541
youssef <sighari.youssef@hotmail.com>
2022-03-15 11:06:34 +0100

    ex article

--- 12.c
+++ 12.c
@@ -1,66 +1,72 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
+    int mois;
 
-int mois;
+    printf("mois= ");
+    scanf("%d", &mois);
 
+    if (mois == 1)
+    {
+        printf("mois de janvier");
+    }
 
+    else if (mois == 2)
+    {
+        printf("mois de fevrier");
+    }
 
-printf("mois= ");
-scanf("%d", &mois);
+    else if (mois == 3)
+    {
+        printf("mois de mars");
+    }
 
+    else if (mois == 4)
+    {
+        printf("mois de avril");
+    }
 
-if(mois==1){
-    printf("mois de janvier");
-}
+    else if (mois == 5)
+    {
+        printf("mois de mai");
+    }
 
-else if(mois==2) { 
-    printf("mois de fevrier");
-}
+    else if (mois == 6)
+    {
+        printf("mois de juin");
+    }
 
-else if(mois==3){
-    printf("mois de mars");
-}
+    else if (mois == 7)
+    {
+        printf("mois de juillet");
+    }
 
-else if(mois==4){
-    printf("mois de avril");
-}
+    else if (mois == 8)
+    {
+        printf("mois de aout");
+    }
 
-else if(mois==5){
-    printf("mois de mai");
-}
+    else if (mois == 9)
+    {
+        printf("mois de september");
+    }
 
-else if(mois==6){
-    printf("mois de juin");
-}
+    else if (mois == 10)
+    {
+        printf("mois de octobre");
+    }
 
-else if(mois==7){
-    printf("mois de juillet");
-}
+    else if (mois == 11)
+    {
+        printf("mois de novembre");
+    }
 
-else if(mois==8){
-    printf("mois de aout");
-}
-
-else if(mois==9){
-    printf("mois de september");
-}
-
-else if(mois==10){
-    printf("mois de octobre");
-}
-
-else if(mois==11){
-    printf("mois de novembre");
-}
-
-else{
-    printf("mois de decembre");
-}
-
-
-
+    else
+    {
+        printf("mois de decembre");
+    }
 
     return 0;
 }
--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-typedef struct etud
-{
-    int nce;
-    char nom[20];
-    char prenom[20];
-    int niveau;
-    float moyenne;
-    int nb_matieres;
-} etud;
 
 typedef struct matiere
 {
@@ -17,7 +8,21 @@ typedef struct matiere
     float note;
 } matiere; 
 
-matiere tab[12];
+
+typedef struct etud
+{
+    int nce;
+    char nom[20];
+    char prenom[20];
+    int niveau;
+    float moyenne;
+    int nb_matieres;
+    matiere tab[12];
+} etud;
+
+
+
+
 
 void saisie_etud(etud *E)
 {
@@ -30,32 +35,60 @@ void saisie_etud(etud *E)
     printf("\ndonner le niveau: ");
     scanf("%d", &(*E).niveau);
     printf("\ndonner le nombre des matieres: ");
-    scanf("%d\n", &(*E).nb_matieres);
+    scanf("%d", &(*E).nb_matieres);
+
+    int i;
+    for(i=0; i<(*E).nb_matieres; i++){
+        printf("le nom de la matiere est:");
+        scanf("%s",(*E).tab[i].nom);
+          printf("la coefficient de la matiere est: ");
+        scanf("%f",&(*E).tab[i].coeff);
+          printf("la note est: ");
+        scanf("%f",&(*E).tab[i].note);
+
+    }
 }
 
-float calculer_moyenne(int nb_matiere)
+void afficher_etud(etud E){
+  printf("le numero du nce est %d\n",E.nce);
+  printf("le nom de l'etudiant est %s\n",E.nom);
+  printf("le prenom de l'etudiant est %s\n",E.prenom);
+  printf("le niveau de l'etudiant est %d\n",E.niveau);
+  printf("le nombre de matieres est %d",E.nb_matieres);
+}
+
+void calculer_moyenne_etudiant(etud *E)
 {
-    struct matiere matiere;
+    
     float s = 0;
-    int i;
-    for (i = 0; i <= 12; i++)
-    {
-        s = s + tab[i].note * tab[i].coeff;
-    }
-
     float s1 = 0;
-    for (i = 0; i <= 12; i++)
+    int i;
+    for (i = 0; i < (*E).nb_matieres; i++)
     {
-        s1 = s1 + tab[i].coeff;
+        s = s + (*E).tab[i].note * E.tab[i].coeff;
+        s1 = s1 + (*E).tab[i].coeff;
     }
 
+   
+    
     float moyenne = s / s1;
 
     return moyenne;
 }
 
+//float calculer_moyenne_classe()
+
+
+
+
+
+
+
 int main()
 {
-
+etud A;
+saisie_etud(&A);
+ afficher_etud(&A);
+printf("moyenne de l'etudiant :%f",calculer_moyenne_etudiant(A));
     return 0;
 }
